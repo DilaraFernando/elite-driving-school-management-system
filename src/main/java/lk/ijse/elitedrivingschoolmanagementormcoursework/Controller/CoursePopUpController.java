7c4dac197334a2337926641ac99d048b99c9d115
Your Name <you@example.com>
2025-09-20 16:02:42 +0530

    20th update CourseManagePage.fxml and CourseManagePage.fxml,StudentPopUpController

--- src/main/java/lk/ijse/elitedrivingschoolmanagementormcoursework/Controller/CoursePopUpController.java
+++ src/main/java/lk/ijse/elitedrivingschoolmanagementormcoursework/Controller/CoursePopUpController.java
@@ -1,4 +1,11 @@
 package lk.ijse.elitedrivingschoolmanagementormcoursework.Controller;
 
+import javafx.event.ActionEvent;
+
 public class CoursePopUpController {
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/lk/ijse/elitedrivingschoolmanagementormcoursework/Controller/InstructorsPopUpController.java
+++ src/main/java/lk/ijse/elitedrivingschoolmanagementormcoursework/Controller/InstructorsPopUpController.java
@@ -2,10 +2,11 @@ package lk.ijse.elitedrivingschoolmanagementormcoursework.Controller;
 
 import javafx.event.ActionEvent;
 
-public class StudentPopUpController {
+public class InstructorsPopUpController {
     public void btnSaveOncAction(ActionEvent actionEvent) {
     }
 
     public void btnUpdateOnAction(ActionEvent actionEvent) {
+        
     }
 }
